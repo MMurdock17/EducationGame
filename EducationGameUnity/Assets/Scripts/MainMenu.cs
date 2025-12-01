ae9bb1a1e06d356e6c920ad1afaf5e695f839ddd
MMurdock17 <mattiemurdock1@gmail.com>
2025-12-01 11:10:07 -0500

    Scripting

--- EducationGameUnity/Assets/Scripts/MainMenu.cs
+++ EducationGameUnity/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
+    public void CreateSetScene()
+	{
+		
+		SceneManager.LoadSceneAsync(8);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void SavedSetsScene()
+	{
+		SceneManager.LoadSceneAsync(13);
+	}
+
 }
